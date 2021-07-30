01237b3e9acaa44fde868058dfefdc0945c4b9fb
5061ksh@naver.com <5061ksh@naver.com>
2021-07-31 00:48:28 +0900

    edit( FragmentMyProject, activity_fragment_my_project.xml, activity_sign_up.xml ) : MyProject 탭 액티비티 구현, xml 음영 효과 적용

--- app/src/main/java/com/example/mainactivity/FragmentMyProject.java
+++ app/src/main/java/com/example/mainactivity/FragmentMyProject.java
@@ -2,10 +2,10 @@ package com.example.mainactivity;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
@@ -15,11 +15,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class FragmentMyProject extends Fragment {
-    private ImageView recentProjectImage;
+    private ImageView recentProjectButton;
     private TextView recentProjectName, recentProjectDescription;
     private Button moreButton;
 
-    public ImageView project1Image, project2Image;
+    public ImageView project1Button, project2Button, project3Button;
     public TextView project1Name, project1Category, project2Name, project2Category, project3Name, project3Category;
 
     public static FragmentMyProject newInstance() {
@@ -31,9 +31,10 @@ public class FragmentMyProject extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_fragment_my_project, container, false);
 
-        recentProjectImage = (ImageView) view.findViewById(R.id.recent_project_image);
-        project1Image = (ImageView) view.findViewById(R.id.project1_image);
-        project2Image = (ImageView) view.findViewById(R.id.project2_image);
+        recentProjectButton = (ImageView) view.findViewById(R.id.recent_project_button);
+        project1Button = (ImageView) view.findViewById(R.id.project1_button);
+        project2Button = (ImageView) view.findViewById(R.id.project2_button);
+        project3Button = (ImageView) view.findViewById(R.id.project3_button);
         recentProjectName = (TextView) view.findViewById(R.id.recent_project_name);
         recentProjectDescription = (TextView) view.findViewById(R.id.recent_project_description);
         project1Name = (TextView) view.findViewById(R.id.project1_name);
@@ -52,6 +53,62 @@ public class FragmentMyProject extends Fragment {
         });
 
         /* 서버나 로컬에서 프로젝트를 생성하면 그 정보를 받아오는 부분 */
+
+        /* 프로젝트 하나를 선택하면 프로젝트 상세 정보 화면을 위해 정보를 넘겨주는 부분 */
+        recentProjectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String projectName = recentProjectName.getText().toString();
+
+                // 프로젝트 세부 정보 화면에서 프로젝트 아이콘 사진을 받아올 떄 식별용으로 사용할 이미지 이름
+                String projectImageName = projectName + "_image";
+
+                // 선택한 프로젝트의 정보를 가지고 세부 정보 화면으로 전환
+                Intent recentProjectIntent = new Intent(getActivity(), ProjectDetailActivity.class);
+                recentProjectIntent.putExtra("projectName", projectName);
+                recentProjectIntent.putExtra("projectImageName", projectImageName);
+                startActivity(recentProjectIntent);
+            }
+        });
+
+        project1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String projectName = project1Name.getText().toString();
+                String projectImageName = projectName + "_image";
+
+                Intent project1Intent = new Intent(getActivity(), ProjectDetailActivity.class);
+                project1Intent.putExtra("projectName", projectName);
+                project1Intent.putExtra("projectImageName", projectImageName);
+                startActivity(project1Intent);
+            }
+        });
+
+        project2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String projectName = project2Name.getText().toString();
+                String projectImageName = projectName + "_image";
+
+                Intent project2Intent = new Intent(getActivity(), ProjectDetailActivity.class);
+                project2Intent.putExtra("projectName", projectName);
+                project2Intent.putExtra("projectImageName", projectImageName);
+                startActivity(project2Intent);
+            }
+        });
+
+        project3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String projectName = project2Name.getText().toString();
+                String projectImageName = projectName + "_image";
+
+                Intent project3Intent = new Intent(getActivity(), ProjectDetailActivity.class);
+                project3Intent.putExtra("projectName", projectName);
+                project3Intent.putExtra("projectImageName", projectImageName);
+                startActivity(project3Intent);
+            }
+        });
 
         return view;
     }
